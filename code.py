eaab257a0e09745e2da1458622a1baa00db41010
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-07-11 15:40:42 -0300

    Fisrt run

--- code.py
+++ code.py
@@ -1,18 +1,21 @@
 END_ATOM = '$'
 Expand_action = 'Expandir'
 unstack_action = 'Dempilhar, avançar'
+epsilon = 'ε'
+
 
 class Analyser:
     def __init__(self):
         self.end_atom = END_ATOM
-        self.atoms = ['id', '+', '*', self.end_atom]
+        self.atoms = ['id', '+', '*', '(', ')', self.end_atom]
         self.non_terminals = ['E', "E'", 'T', "T'", 'F']
         self.stack = Stack()
 
         self.entry = [self.end_atom]
         self.actual_char = self.end_atom
         self.actual_char_idx = 0
-        pass
+
+        self.table = self.__create_table()
 
     def analyse(self, entry):
         if not self.non_terminals: raise Exception('non terminals are empty')
@@ -30,7 +33,7 @@ class Analyser:
             initial_entry = self.__get_entry_state()
 
             if self.__is_non_terminal(actual_production):
-                self.stack.top()
+                self.stack.pop()
 
                 actual_actions = Expand_action
 
@@ -70,6 +73,46 @@ class Analyser:
         self.entry = entry
         self.actual_char = self.entry[0]
         self.actual_char_idx = 0
+
+    def __create_table(self):
+        self.table = dict()
+        self.table[self.non_terminals[0] + self.atoms[0]] = ['T', "E'"]
+        self.table[self.non_terminals[1] + self.atoms[0]] = []
+        self.table[self.non_terminals[2] + self.atoms[0]] = ['F', "T'"]
+        self.table[self.non_terminals[3] + self.atoms[0]] = []
+        self.table[self.non_terminals[4] + self.atoms[0]] = ['id']
+
+        self.table[self.non_terminals[0] + self.atoms[1]] = []
+        self.table[self.non_terminals[1] + self.atoms[1]] = ['T', "E'"]
+        self.table[self.non_terminals[2] + self.atoms[1]] = []
+        self.table[self.non_terminals[3] + self.atoms[1]] = [epsilon]
+        self.table[self.non_terminals[4] + self.atoms[1]] = []
+
+        self.table[self.non_terminals[0] + self.atoms[2]] = []
+        self.table[self.non_terminals[1] + self.atoms[2]] = []
+        self.table[self.non_terminals[2] + self.atoms[2]] = []
+        self.table[self.non_terminals[3] + self.atoms[2]] = ['*', 'F', "T'"]
+        self.table[self.non_terminals[4] + self.atoms[2]] = []
+
+        self.table[self.non_terminals[0] + self.atoms[3]] = ['T', "E'"]
+        self.table[self.non_terminals[1] + self.atoms[3]] = []
+        self.table[self.non_terminals[2] + self.atoms[3]] = ['F', "T'"]
+        self.table[self.non_terminals[3] + self.atoms[3]] = []
+        self.table[self.non_terminals[4] + self.atoms[3]] = ['(', 'E', ')']
+
+        self.table[self.non_terminals[0] + self.atoms[4]] = []
+        self.table[self.non_terminals[1] + self.atoms[4]] = [epsilon]
+        self.table[self.non_terminals[2] + self.atoms[4]] = []
+        self.table[self.non_terminals[3] + self.atoms[4]] = [epsilon]
+        self.table[self.non_terminals[4] + self.atoms[4]] = []
+
+        self.table[self.non_terminals[0] + self.atoms[5]] = []
+        self.table[self.non_terminals[1] + self.atoms[5]] = [epsilon]
+        self.table[self.non_terminals[2] + self.atoms[5]] = []
+        self.table[self.non_terminals[3] + self.atoms[5]] = [epsilon]
+        self.table[self.non_terminals[4] + self.atoms[5]] = []
+
+        return self.table
 
     def __print_current_action(self, stack, entry, action, production, next_productions):
         if next_productions is not None:
