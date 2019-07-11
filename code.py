b250f192ec162b8fa5d0baf4dadbb237b4df341c
Carlos Matheus Barros da Silva <carlosmatheusbs@gmail.com>
2019-07-11 14:12:31 -0300

    Initial commit

---
+++ code.py
@@ -0,0 +1,31 @@
+
+
+def solve():
+    """
+    Alg to the problem
+    :return:
+    """
+    w = sentence # || $ ???
+    p = w[0]
+    stack = Stack()
+    while(X is not None): # while X is not $
+        x = stack.top()
+        a = *p # ???
+        if X = terminal
+
+
+class Stack:
+    """
+    Stack class
+    """
+    def __init__(self):
+        self.s = []
+
+    def pop(self):
+        return self.s.pop()
+
+    def top(self):
+        return self.s[-1]
+
+    def push(self, elm):
+        self.s.append(elm)
